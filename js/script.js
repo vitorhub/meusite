adf6c910c1fcf0e4ccd80160a1ec73a2f468710b
Vitor Falcao <joaofalcao.33@outlook.com>
2022-07-28 16:19:15 -0300

    site tema mar

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const menu = document.getElementById("checkbox-menu");
+const navegacao = document.getElementById("navegacao");
+
+menu.addEventListener("click",
+    () => {
+        if (menu.checked == true) {
+            navegacao.classList.toggle("mostrar");
+        } else {
+            navegacao.classList.toggle("mostrar");
+        }
+    }
+)
+
+
+const item = document.querySelectorAll("[data-anime]");
+const animeScroll = ()=>{
+    const windowTop = window.pageYOffset
+    + window.innerHeight * 0.85;
+    /* window.pageYOffset começa a 200 da base */
+    /* window.innerHeight 635 */
+
+    item.forEach(
+        (element)=>{
+            if(windowTop > element.offsetTop){
+                /* element.offsetTop 620 */
+                element.classList.add("animate");
+            }else{
+                element.classList.remove("animate");
+            }
+        }
+    )
+}
+
+animeScroll();
+window.addEventListener("scroll",
+    ()=>{animeScroll() }
+)
